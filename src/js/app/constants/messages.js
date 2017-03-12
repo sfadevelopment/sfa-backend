fedd5a56151ac2796d2ce3cb8d32a2f685f993b3
lgorewit <lgorewit@users.noreply.github.com>
2017-03-12 14:00:39 -0700

    Add files via upload

---
+++ src/js/app/constants/messages.js
@@ -0,0 +1,4 @@
+angular.module('SFA')
+    .constant('SFA_MESSAGES', {
+       file_too_big: 'Maximum image file size is 2 Megabytes. Some of the images were not uploaded. Please resize or compress the images.'
+    });
